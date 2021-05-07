f993c15417452e56ef8318c4535632d4961c344b
DESKTOP-5367GUT\노수빈 <eoghkqks@naver.com>
2021-05-07 17:34:01 +0900

    upload python3_01

---
+++ Level1/python3_01.py
@@ -0,0 +1,9 @@
+"""
+n개의 배열에서 n/2개로 만들 수 있는 조합 중 원소가 가장 다양할 때 원소의 개
+"""
+
+def solution(nums):
+    unique_list = set(nums)
+    select_len = len(nums)/2
+    max_len = len(unique_list)
+    return max_len if max_len <= select_len else select_len
